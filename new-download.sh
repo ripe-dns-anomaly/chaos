e31e30006a3a1d16a8bfae6e5e94009c44843633
rschmidt <ricardo@Ricardos-MacBook-Pro.local>
2017-04-21 14:58:27 +0200

    final download file

--- new-download.sh
+++ new-download.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-declare -a root_letter=( "a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" );
-declare -a msn_id_v4=( "10309" "10310" "10311" "10312" "10313" "10304" "10314" "10315" "10305" "10316" "10301" "10308" "10306" );
-
 declare -a root_letter=( "k" );
 declare -a msn_id_v4=( "10301" );
 
@@ -13,34 +10,51 @@ path_atlas_json="atlas_input/";
 path_json_result="json_result/";
 
 
-if [ $# -ne 2 ]; then
-  echo "ERROR: see usage for arguments.";
+if [ $# -ne 4 ]; then
+  echo "ERROR! number of arguments is not correct. Usage:";
+  echo "    #1: start time of measurement";
+  echo "    #2: stop time of measurement";
+  echo "    #3: bit size in seconds";
+  echo "    #4: measurement ID";
   exit;
 fi;
 
 # This are the times specified for the interval to retrieve CHAOS results
-init_time=$1
+init_time=$1;
 stop_time=$2;
+# Bin size for measurments
+bin_size=$3;
+# This is the measurement ID of CHAOS queries
+msn_id=$4;
 
+error=`echo "$init_time $stop_time" | awk '{if($1>$2) print "1"; else print "0";}'`;
+if [ $error -eq 1 ]; then
+  echo "ERROR! start time should be smaller than stop time";
+  exit;
+fi;
 
-# Loop to download and process per root letter
-count_letter=-1;
-for l in ${root_letter}; do
+let cur_stop_time=$init_time+$bin_size;
+let cur_init_time=$init_time;
 
-  count_letter+=1;
+while [ $cur_stop_time -le $stop_time ]; do
+
+  echo "*** current period from $cur_init_time to $cur_stop_time";
 
   #
   # Download JSON
-  msn_src="https://atlas.ripe.net/api/v2/measurements/${msn_id_v4[$count_letter]}/results?start=${init_time}&stop=${stop_time}&format=json";
-  dst_file="atlas_input/${l}-${init_time}.json";
+  msn_src="https://atlas.ripe.net/api/v2/measurements/${msn_id}/results?start=${cur_init_time}&stop=${cur_stop_time}&format=json";
+  dst_file="atlas_input/letter-${cur_init_time}.json";
   rm -f ${dst_file};
   curl -o $dst_file $msn_src;
+
+  let cur_init_time=$cur_stop_time;
+  let cur_stop_time+=$bin_size;
 
   #
   # Parse JSON to csv; output:
   #   1       | 2             | 3   | 4
   #   prb ID  | CHAOS string  | RTT | RCODE
-  parsed_file="json_result/${l}-${init_time}.csv";
+  parsed_file="json_result/letter-${cur_init_time}.csv";
   rm -f ${parsed_file};
   python json-parser/parser.py ${dst_file} ${parsed_file};
 
@@ -49,7 +63,7 @@ for l in ${root_letter}; do
   #   2       | 2             | 3   | 4
   #   prb ID  | CHAOS string  | RTT | RCODE
   rm -f ${clean_file};
-  clean_file="json_result/${l}-${init_time}-clean.csv";
+  clean_file="json_result/letter-${cur_init_time}-clean.csv";
   awk '
     {
       if (NF > 1) {
@@ -69,41 +83,41 @@ for l in ${root_letter}; do
 
   #
   # calculate per-letter statistics
-  stats_file_time="stats_result/${l}-${init_time}.csv";
-  stats_file_all="stats_result/${l}.csv";
+  stats_file_time="stats_result/letter-${cur_init_time}.csv";
+  stats_file_all="stats_result/letter.csv";
 
   _stat_number_probes=`cut -f 1 -d ' ' ${clean_file} | sort -n | uniq | wc -l | awk '{print $1;}'`;
-	_stat_number_sites=`cut -f 2 -d ' ' ${clean_file} | sort | uniq | wc -l | awk '{print $1;}'`;
+  _stat_number_sites=`cut -f 2 -d ' ' ${clean_file} | sort | uniq | wc -l | awk '{print $1;}'`;
   _stat_number_replies=`awk 'BEGIN{tot=0;}{if ($4==0) tot++;}END{print tot;}' ${clean_file}`;
   _stat_number_queries=`wc -l ${clean_file} | awk '{print $1;}'`;
 
-	_stat_rtt=`cut -f 3 -d ' ' ${clean_file} |\
-		sort -n |\
-		awk '{
-				_rtt[++i] = $1;
-			}
-			END {
-				if (i%2 == 0)
-					_50_rtt = (_rtt[i/2]+_rtt[(i/2)+1])/2;
-				else
-					_50_rtt = _rtt[int(i/2)+1];
+  _stat_rtt=`cut -f 3 -d ' ' ${clean_file} |\
+    sort -n |\
+    awk '{
+        _rtt[++i] = $1;
+      }
+      END {
+        if (i%2 == 0)
+          _50_rtt = (_rtt[i/2]+_rtt[(i/2)+1])/2;
+        else
+          _50_rtt = _rtt[int(i/2)+1];
 
-				_25_rtt = _rtt[int(i*0.25)]; 
-				_75_rtt = _rtt[int(i*0.75)]; 
-				_90_rtt = _rtt[int(i*0.90)]; 
+        _25_rtt = _rtt[int(i*0.25)]; 
+        _75_rtt = _rtt[int(i*0.75)]; 
+        _90_rtt = _rtt[int(i*0.90)]; 
 
-				printf "%.04f,%.04f,%.04f,%.04f", _25_rtt, _50_rtt, _75_rtt, _90_rtt;
-			}'`;
+        printf "%.04f,%.04f,%.04f,%.04f", _25_rtt, _50_rtt, _75_rtt, _90_rtt;
+      }'`;
 
   if [ ! -f $stats_file_all ]; then
-  	echo "timestamp,nProbes,nSites,nQueries,nResponses,q25RTT,q50RTT,q75RTT,q90RTT" >> $stats_file_all;
+    echo "timestamp,nProbes,nSites,nQueries,nResponses,q25RTT,q50RTT,q75RTT,q90RTT" >> $stats_file_all;
   fi;
-	echo "$init_time,$_stat_number_probes,$_stat_number_sites,$_stat_number_queries,$_stat_number_replies,$_stat_rtt" >> $stats_file_all;
+  echo "$init_time,$_stat_number_probes,$_stat_number_sites,$_stat_number_queries,$_stat_number_replies,$_stat_rtt" >> $stats_file_all;
 
 
   #
   # calculate per-site statistics
-  out_path="stats_result/${l}";
+  out_path="stats_result/sites";
   gawk '
     {
       site_hit[$2]++;
@@ -131,8 +145,7 @@ for l in ${root_letter}; do
         if (system("test -f " output_file) != 0)
           printf "timestamp,nProbes,nQueries,nResponses,q25RTT,q50RTT,q75RTT,q90RTT\n" > output_file;
 
-        #
-        ## NUMBER OF QUERIES (3rd column) STILL HAS TO BE COMPUTED
+        ## TODO: NUMBER OF QUERIES (3rd column) STILL HAS TO BE COMPUTED ##
 
         printf "%s,%d,NA,%d,%.04f,%.04f,%.04f,%.04f\n",
                  TS, _probes,site_hit[s],
@@ -145,9 +158,8 @@ for l in ${root_letter}; do
 
   rm -f ${dst_file} ${parsed_file};
 
+
 done;
-
-
 
 
 
